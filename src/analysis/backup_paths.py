41b2c63cfa6ef13e588950060485ce051a4bf0bf
Rakesh Kumar <gopchandani@gmail.com>
2014-09-17 09:09:07 -0700

    Giving analysis some shape

--- src/analysis/backup_paths.py
+++ src/analysis/backup_paths.py
@@ -5,24 +5,37 @@ import networkx as nx
 from model.model import Model
 
 
-model = Model()
-graph = model.get_node_graph()
-host_ids = model.get_host_ids()
-switch_ids = model.get_switch_ids()
+class BackupPaths:
+    def __init__(self):
+        self.model = Model()
+        self.graph = self.model.get_node_graph()
+        self.host_ids = self.model.get_host_ids()
+        self.switch_ids = self.model.get_switch_ids()
 
-print "Hosts in the graph:", host_ids
-print "Switches in the graph:", switch_ids
-print "Number of nodes add in the graph:", graph.number_of_nodes()
+    def analyze_all_node_pairs(self):
 
-print "Checking for backup paths between all possible host pairs..."
-for src_host_id in host_ids:
-    for dst_host_id in host_ids:
-        print 'Path from', src_host_id, 'to', dst_host_id
+        print "Hosts in the graph:", self.host_ids
+        print "Switches in the graph:", self.switch_ids
+        print "Number of nodes add in the graph:", self.graph.number_of_nodes()
 
-        print "Topological paths:"
-        asp = nx.all_simple_paths(graph, source=src_host_id, target=dst_host_id)
-        for p in asp:
-            print p
+        print "Checking for backup paths between all possible host pairs..."
+        for src_host_id in self.host_ids:
+            for dst_host_id in self.host_ids:
+                print 'Path from', src_host_id, 'to', dst_host_id
 
-            for node in p:
-                print "node id", node, "graph node", graph.node[node]
+                print "Topological paths:"
+                asp = nx.all_simple_paths(self.graph, source=src_host_id, target=dst_host_id)
+                for p in asp:
+                    print p
+
+                    for node in p:
+                        print "node id", node, "graph node", self.graph.node[node]
+
+
+def main():
+    bp = BackupPaths()
+    bp.analyze_all_node_pairs()
+
+
+if __name__ == "__main__":
+    main()
