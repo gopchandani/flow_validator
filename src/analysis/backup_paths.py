5807ebf0f8ad30f000e1a1c959639f5c4f741d64
Rakesh Kumar <gopchandani@gmail.com>
2014-09-16 20:30:21 -0700

    Setting up for path analysis

--- src/analysis/backup_paths.py
+++ src/analysis/backup_paths.py
@@ -15,14 +15,21 @@ switch_ids = model.get_switch_ids()
 
 print "Hosts in the graph:", host_ids
 print "Switches in the graph:", switch_ids
-
-# Print out graph info as a sanity check
 print "Number of nodes add in the graph:", graph.number_of_nodes()
 
-print "Topological paths from 10.0.0.1 -> 10.0.0.4"
-asp = nx.all_simple_paths(graph, source="10.0.0.1", target="10.0.0.4")
-for p in asp:
-    print p
+print "Checking for backup paths between all possible host pairs..."
+for src_host_id in host_ids:
+    for dst_host_id in host_ids:
+        print 'Path from', src_host_id, 'to', dst_host_id
+
+        print "Topological paths:"
+        asp = nx.all_simple_paths(graph, source=src_host_id, target=dst_host_id)
+        for p in asp:
+            print p
+
+            for node in p:
+                print p
+
 
 # write json formatted data to use in visualization
 d = json_graph.node_link_data(graph)
