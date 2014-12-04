b85d5716ddb2aff7c3006b05946a3aea9f40083e
Rakesh Kumar <gopchandani@gmail.com>
2014-12-04 11:31:52 -0800

    hodge-podge

--- src/analysis/net_switch.py
+++ src/analysis/net_switch.py
@@ -41,54 +41,52 @@ class NetSwitch:
     # This function populates the port graph with edges and match state
     def populate_port_graph(self, port):
 
-        # Capture the action_list for ports before and after a table.
+        # Before this function gets triggered for a specific destination host, we need some
+        # abstract, generic analysis prepared that applies to more than this host. This pre-analysis need to consider
+        # how a wildcard flow moves from one port to another in the switch through all the tables.
+
+        # What I just described above is essentially what a transfer function does right now. If we keep that inside it,
+        # Then the transfer function essentially needs to be pre-computed once and then queried by ports as hosts for analysis
+        # get added
+
+        # Capture the action_list for ports before and after a table for a wildcard match,
         # The path from one external facing port to another goes through a sequence of action lists
         # This breaks when the next action depends on changes that have already been made to the header
         # aka apply-action
-
-        # See what actions across the tables does this match attract.
-        # Some of these actions would be a function of in_port and some won't be. But the match we should try
-        # should have in_port set to "all"
-
-        # That is to do this from the prior computation instead of doing transfer function over and it and over again
-
         # The same nature of things also applies when we are traversing switch boundaries, except the action_list
         # is empty
+        # Each switch port has entries for all the destination switches +
+        # hosts that are directly to the switch as destinations
 
-
-        #Each switch port has entries for all the destination switches + hosts that are directly to it as destinations
+        # for destination in port.destination_match:
+        #
+        #     destination_match = deepcopy(port.destination_match[destination])
+        #
+        #     destination_match.in_port = "all"
+        #     for i in range(len(port.sw.flow_tables)):
+        #
+        #         table_port = self._get_table_port(port.sw.node_id, i)
+        #         print table_port.port_id
+        #         print port.sw.flow_tables[i].table_id
+        #
+        #         hpm_flow, intersection = port.sw.flow_tables[i].get_highest_priority_matching_flow(destination_match)
+        #
+        #
+        #         #TODO: Need something on the lines of get_hpm_flow_actions_list
+        #         print hpm_flow.applied_actions
+        #         print hpm_flow.written_actions
 
         for destination in port.destination_match:
 
-            destination_match = deepcopy(port.destination_match[destination])
+            # See what other ports on this switch can reach this port and with what match
+            for sw_port in port.sw.ports.values():
 
-            destination_match.in_port = "all"
-            for i in range(len(port.sw.flow_tables)):
-    
-                table_port = self._get_table_port(port.sw.node_id, i)    
-                print table_port.port_id
-                print port.sw.flow_tables[i].table_id
+                if sw_port != port:
+                    destination_match = deepcopy(port.destination_match[destination])
+                    destination_match.in_port = sw_port.port_number
+                    output_match = port.sw.transfer_function(destination_match)
+                    sw_port.destination_match[destination] = output_match[port.port_number]
 
-                hpm_flow, intersection = port.sw.flow_tables[i].get_highest_priority_matching_flow(destination_match)
-
-
-                #TODO: Need something on the lines of get_hpm_flow_actions_list
-                print hpm_flow.action_list
-
-
-            #
-            # # See what other ports on this switch can reach this port and with what match
-            # for port in port.sw.ports.values():
-            #
-            #     if port != port:
-            #         destination_match = port.host_match
-            #         destination_match.in_port = port.port_number
-            #         output_match = port.sw.transfer_function(destination_match)
-            #         port.destination_match[port.port_id] = output_match[port.port_number]
-            #
-
-
-        # TODO: Need to have a method for switch which would do this,
         # Takes a destination port, and source port and computes precisely just that.
     
         # Go a level higher, i.e. Go to the ports that are physically 
