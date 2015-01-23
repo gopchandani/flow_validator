429042851b0922348aabfbc3f4b02707c1bc52de
Rakesh Kumar <gopchandani@gmail.com>
2015-01-23 15:35:34 -0800

    Adding controller_man

---
+++ src/mininet/controller_man.py
@@ -0,0 +1,17 @@
+__author__ = 'Shane Rogers'
+
+
+class ControllerMan():
+
+    def __init__(self, num_cons):
+        pass
+
+    def get_next(self):
+        print 'here '
+
+def main():
+    cm = ControllerMan(5)
+    cm.get_next()
+    
+if __name__ == "__main__":
+    main()
