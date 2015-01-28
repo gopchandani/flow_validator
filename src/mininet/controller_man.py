0ce4e98cd78ac0a238b8884c9a336ab2c20bdf44
Shane <sprogrs2@illinois.edu>
2015-01-28 12:57:25 -0800

    made system calls, argv passing still broken.

--- src/mininet/controller_man.py
+++ src/mininet/controller_man.py
@@ -1,21 +1,26 @@
+import os
 __author__ = 'Shane Rogers'
 
+num_cons = 0
 
 class ControllerMan(num_cons):
 
     def __init__(self, num_cons):
-    	for i in range num_cons:
+    	print num_cons
+    	for i in range (num_cons):
     		port = 6630 + i
-    		sudo docker run -d -t -i -p=port:6633 controller distribution-karaf-0.2.1-Helium-SR1/bin/karaf
-        	sudo docker stop $(sudo docker ps -q)
+    		os.system('sudo docker run -d -t -i -p=port:6633 controller distribution-karaf-0.2.1-Helium-SR1/bin/karaf')
+        	os.system('sudo docker stop $(sudo docker ps -q)')
         	return port
 
     def get_next(self):
         print 'here '
 
 def main():
-    cm = ControllerMan(argv)
-    cm.get_next()
+	num_cons = argv[1]
+	print num_cons
+	cm = ControllerMan(num_cons)
+	cm.get_next()
     
 if __name__ == "__main__":
     main()
