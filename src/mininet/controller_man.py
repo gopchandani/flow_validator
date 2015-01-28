edf359f31bd2cfd274862c645aee776b63000cab
Shane <sprogrs2@illinois.edu>
2015-01-28 12:48:15 -0800

    established docker loop, untested

--- src/mininet/controller_man.py
+++ src/mininet/controller_man.py
@@ -1,16 +1,20 @@
 __author__ = 'Shane Rogers'
 
 
-class ControllerMan():
+class ControllerMan(num_cons):
 
     def __init__(self, num_cons):
-        pass
+    	for i in range num_cons:
+    		port = 6630 + i
+    		sudo docker run -d -t -i -p=port:6633 controller distribution-karaf-0.2.1-Helium-SR1/bin/karaf
+        	sudo docker stop $(sudo docker ps -q)
+        	return port
 
     def get_next(self):
         print 'here '
 
 def main():
-    cm = ControllerMan(5)
+    cm = ControllerMan(argv)
     cm.get_next()
     
 if __name__ == "__main__":
