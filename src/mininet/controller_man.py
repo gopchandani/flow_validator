3900b3ca511e142a705669102c8762d82a9e0535
Shane Rogers <shane@shanerogers.info>
2015-02-04 09:58:27 -0600

    comments

--- src/mininet/controller_man.py
+++ src/mininet/controller_man.py
@@ -14,9 +14,13 @@ class ControllerMan():
     		os.system('sudo docker run -d -t -i -p=port:6633 controller distribution-karaf-0.2.1-Helium-SR1/bin/karaf')
         	os.system('sudo docker stop $(sudo docker ps -q)')
         	ports.append(port)
+        	#need array of container ids too
 
     def get_next(self):
         print 'here '
+        #this will unpause the next container and return a port number
+        #it will also rmove the container ID from the array
+        #will eventually start a new container and add it to the back of array
 
 def main():
 	num_cons = int(sys.argv[1])
