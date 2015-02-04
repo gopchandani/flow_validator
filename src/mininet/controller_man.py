62939556c83a6f2d563a9d8191d7d3f3e2d736f9
Shane <sprogrs2@illinois.edu>
2015-02-04 14:53:11 -0800

    tyring to close readlines()

--- src/mininet/controller_man.py
+++ src/mininet/controller_man.py
@@ -5,20 +5,25 @@ __author__ = 'Shane Rogers'
 
 num_cons = 0
 ports = []
+data = []
 
 class ControllerMan():
 
-    def __init__(self, num_cons):
+    def get_container_ids(self):
+   		data.append(sys.stdin.readlines())
+   		subprocess.call("sudo docker ps -q", shell=True)
+   		sys.stdin.close() ##need something here to tell readlines() to stop. 
 
+    def __init__(self, num_cons):
     	for i in range (num_cons):
     		new_port = int(6630 + i)
     		command = 'sudo docker run -d -t -i -p=%s:6633 controller distribution-karaf-0.2.1-Helium-SR1/bin/karaf'%str(new_port) 
     		os.system(command)
-        	subprocess.call("sudo docker ps -q", shell=True)
         	ports.append(new_port)
-        	#print p
-        	#need array of container ids too
 
+        self.get_container_ids()
+        print "Got here"
+        print data
 
 
     def get_next(self):
@@ -34,7 +39,7 @@ class ControllerMan():
 def main():
 	num_cons = int(sys.argv[1])
 	cm = ControllerMan(num_cons)
-	cm.kill_all()
+	#cm.kill_all()
 	#cm.get_next()
     
 if __name__ == "__main__":
