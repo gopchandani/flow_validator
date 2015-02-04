8deb9748de0e6816acf64e225ddf096a4dcd05ff
Shane Rogers <shane@shanerogers.info>
2015-02-04 09:49:35 -0600

    fixed port arrays

--- src/mininet/controller_man.py
+++ src/mininet/controller_man.py
@@ -3,6 +3,7 @@ import sys
 __author__ = 'Shane Rogers'
 
 num_cons = 0
+ports = []
 
 class ControllerMan():
 
@@ -12,15 +13,15 @@ class ControllerMan():
     		port = 6630 + i
     		os.system('sudo docker run -d -t -i -p=port:6633 controller distribution-karaf-0.2.1-Helium-SR1/bin/karaf')
         	os.system('sudo docker stop $(sudo docker ps -q)')
-        	return port
+        	ports.append(port)
 
     def get_next(self):
         print 'here '
 
 def main():
-	num_cons = sys.argv[1]
+	num_cons = int(sys.argv[1])
 	print num_cons
-	#cm = ControllerMan(num_cons)
+	cm = ControllerMan(num_cons)
 	#cm.get_next()
     
 if __name__ == "__main__":
