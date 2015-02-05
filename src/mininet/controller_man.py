e2686bf15b6fc5c25a08298350db140d88d29f1e
Shane <sprogrs2@illinois.edu>
2015-02-05 12:55:19 -0800

    spins up, stops, stores port and id

--- src/mininet/controller_man.py
+++ src/mininet/controller_man.py
@@ -9,21 +9,34 @@ data = []
 
 class ControllerMan():
 
-    def get_container_ids(self):
-   		data.append(sys.stdin.readlines())
-   		subprocess.call("sudo docker ps -q", shell=True)
-   		sys.stdin.close() ##need something here to tell readlines() to stop. 
+#    def get_container_ids(self):
+#   		data.append(sys.stdin.readline())
+#   		subprocess.call("sudo docker ps -q", shell=True)
+#   		sys.stdin.close() ##need something here to tell readlines() to stop. 
 
     def __init__(self, num_cons):
     	for i in range (num_cons):
     		new_port = int(6630 + i)
-    		command = 'sudo docker run -d -t -i -p=%s:6633 controller distribution-karaf-0.2.1-Helium-SR1/bin/karaf'%str(new_port) 
-    		os.system(command)
         	ports.append(new_port)
 
-        self.get_container_ids()
-        print "Got here"
-        print data
+    		start_command = 'sudo docker run -d -t -i -p=%s:6633 controller distribution-karaf-0.2.1-Helium-SR1/bin/karaf'%str(new_port) 
+
+    		os.system(start_command)
+    		proc = subprocess.Popen(['sudo', 'docker', 'ps', '-q'], stdout=subprocess.PIPE)
+    		while True:
+    			this_id = proc.stdout.readline()
+    			if this_id != '':
+    				print this_id
+    				data.append(this_id)
+		    		stop_command = 'sudo docker stop %s'%str(this_id)
+    				os.system(stop_command)
+    			else:
+    				break
+
+
+#        self.get_container_ids()
+        
+        
 
 
     def get_next(self):
@@ -39,6 +52,12 @@ class ControllerMan():
 def main():
 	num_cons = int(sys.argv[1])
 	cm = ControllerMan(num_cons)
+
+	for i in range (num_cons):
+		print "Container with port number",
+		print ports[i],
+		print "has container id",
+		print data[i]
 	#cm.kill_all()
 	#cm.get_next()
     
