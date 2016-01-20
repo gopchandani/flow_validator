fd8a66d12783b935a2c97bbd6b4bdf9a55d119ff
Rakesh Kumar <gopchandani@gmail.com>
2016-01-20 14:24:11 -0800

    Skewing the sampling by explicitly using only those links that are vulnerable after one link failure.

--- src/experiments/host_connectivity_monte_carlo.py
+++ src/experiments/host_connectivity_monte_carlo.py
@@ -132,9 +132,9 @@ def main():
     controller = "ryu"
 
     fanout = 2
-    core = 2
+    core = 1
     total_number_of_ports_to_synthesize = 1
-    numbers_of_monte_carlo_runs = [1]#[10, 20, 30]
+    numbers_of_monte_carlo_runs = [2]#[10, 20, 30]
 
     exp = MonteCarlo(num_iterations,
                      load_config,
