db4eb0b3a83d82b72120d4c72b7c8e9ac02141a4
Rakesh Kumar <gopchandani@gmail.com>
2016-06-23 10:29:25 -0700

    Writing a test proper. Need to figure out a more efficient way of writing these tests.

--- src/model/test_switch_port_graph.py
+++ src/model/test_switch_port_graph.py
@@ -1,8 +1,10 @@
 import unittest
+from collections import defaultdict
 
 from switch_port_graph import SwitchPortGraph
 from network_graph import NetworkGraph
 from experiments.network_configuration import NetworkConfiguration
+from traffic import Traffic
 
 
 class TestSwitchPortGraph(unittest.TestCase):
@@ -24,50 +26,65 @@ class TestSwitchPortGraph(unittest.TestCase):
         self.ring_swpg.init_switch_port_graph()
         self.ring_swpg.compute_switch_admitted_traffic()
 
-    def test_one_port_failure_at_a_time(self, verbose=False):
+    def test_admitted_traffic(self, verbose=False):
 
-        test_passed = True
+        expected_graph_ats = defaultdict(defaultdict)
 
-        # Loop over ports of the switch and fail and restore them one by one
-        for testing_port_number in self.ring_swpg.sw.ports:
+        for src in self.ring_swpg.boundary_ingress_nodes:
+            for dst in self.ring_swpg.boundary_egress_nodes:
+                expected_graph_ats[src][dst] = Traffic(init_wildcard=True)
 
-            print "testing_port_number:", testing_port_number
+        computed_graph_ats = self.ring_swpg.get_graph_ats()
+        all_graph_ats_equal = self.ring_swpg.compare_graph_ats(computed_graph_ats,
+                                                               expected_graph_ats,
+                                                               verbose)
 
-            testing_port = self.ring_swpg.sw.ports[testing_port_number]
+        self.assertEqual(all_graph_ats_equal, True)
 
-            graph_paths_before = self.ring_swpg.get_graph_paths(verbose)
-            graph_ats_before = self.ring_swpg.get_graph_ats()
-
-            testing_port.state = "down"
-            end_to_end_modified_edges = self.ring_swpg.update_admitted_traffic_due_to_port_state_change(testing_port_number,
-                                                                                                   "port_down")
-
-            graph_paths_intermediate = self.ring_swpg.get_graph_paths(verbose)
-            graph_ats_intermediate = self.ring_swpg.get_graph_ats()
-
-            testing_port.state = "up"
-            end_to_end_modified_edges = self.ring_swpg.update_admitted_traffic_due_to_port_state_change(testing_port_number,
-                                                                                                   "port_up")
-
-            graph_paths_after = self.ring_swpg.get_graph_paths(verbose)
-            graph_ats_after = self.ring_swpg.get_graph_ats()
-
-            all_graph_paths_equal = self.ring_swpg.compare_graph_paths(graph_paths_before,
-                                                                  graph_paths_after,
-                                                                  verbose)
-
-            if not all_graph_paths_equal:
-                test_passed = all_graph_paths_equal
-                print "Test Failed."
-
-            all_graph_ats_equal = self.ring_swpg.compare_graph_ats(graph_ats_before,
-                                                              graph_ats_after,
-                                                              verbose)
-            if not all_graph_ats_equal:
-                test_passed = all_graph_ats_equal
-                print "Test Failed."
-
-        return test_passed
+    # def test_one_port_failure_at_a_time(self, verbose=False):
+    #
+    #     test_passed = True
+    #
+    #     # Loop over ports of the switch and fail and restore them one by one
+    #     for testing_port_number in self.ring_swpg.sw.ports:
+    #
+    #         print "testing_port_number:", testing_port_number
+    #
+    #         testing_port = self.ring_swpg.sw.ports[testing_port_number]
+    #
+    #         graph_paths_before = self.ring_swpg.get_graph_paths(verbose)
+    #         graph_ats_before = self.ring_swpg.get_graph_ats()
+    #
+    #         testing_port.state = "down"
+    #         end_to_end_modified_edges = self.ring_swpg.update_admitted_traffic_due_to_port_state_change(testing_port_number,
+    #                                                                                                "port_down")
+    #
+    #         graph_paths_intermediate = self.ring_swpg.get_graph_paths(verbose)
+    #         graph_ats_intermediate = self.ring_swpg.get_graph_ats()
+    #
+    #         testing_port.state = "up"
+    #         end_to_end_modified_edges = self.ring_swpg.update_admitted_traffic_due_to_port_state_change(testing_port_number,
+    #                                                                                                "port_up")
+    #
+    #         graph_paths_after = self.ring_swpg.get_graph_paths(verbose)
+    #         graph_ats_after = self.ring_swpg.get_graph_ats()
+    #
+    #         all_graph_paths_equal = self.ring_swpg.compare_graph_paths(graph_paths_before,
+    #                                                               graph_paths_after,
+    #                                                               verbose)
+    #
+    #         if not all_graph_paths_equal:
+    #             test_passed = all_graph_paths_equal
+    #             print "Test Failed."
+    #
+    #         all_graph_ats_equal = self.ring_swpg.compare_graph_ats(graph_ats_before,
+    #                                                           graph_ats_after,
+    #                                                           verbose)
+    #         if not all_graph_ats_equal:
+    #             test_passed = all_graph_ats_equal
+    #             print "Test Failed."
+    #
+    #     return test_passed
 
 if __name__ == '__main__':
     unittest.main()
