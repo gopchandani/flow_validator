add068fd4c8771c6819aef2310f09f962ed5331d
Rakesh Kumar <gopchandani@gmail.com>
2016-07-02 14:45:20 -0700

    Adding another test that fails. :)

--- src/tests/test_network_port_graph.py
+++ src/tests/test_network_port_graph.py
@@ -3,39 +3,28 @@ import os
 
 from model.traffic import Traffic
 from model.network_port_graph import NetworkPortGraph
-
 from experiments.network_configuration import NetworkConfiguration
 
 
 class TestNetworkPortGraph(unittest.TestCase):
 
-    def setUp(self):
-
-        self.nc = NetworkConfiguration("ryu",
-                                  "ring",
-                                  {"num_switches": 4,
-                                   "num_hosts_per_switch": 1},
-                                  conf_root=os.path.dirname(__file__) + "/",
-                                  synthesis_name="AboresceneSynthesis",
-                                  synthesis_params={"apply_group_intents_immediately": True})
-
-        self.ng = self.nc.setup_network_graph(mininet_setup_gap=1, synthesis_setup_gap=1)
-
-        sw = self.ng.get_node_object("s1")
-        self.ring_npg = NetworkPortGraph(self.ng, True)
-        self.ring_npg.init_network_port_graph()
+    @classmethod
+    def attach_hosts_port_nodes_with_npg(cls, ng, npg):
 
         # Attach a destination port for each host.
-        for host_id in self.ng.host_ids:
+        for host_id in ng.host_ids:
 
-            host_obj = self.ng.get_node_object(host_id)
-            host_obj.switch_ingress_port = self.ring_npg.get_node(host_obj.switch_id +
-                                                                  ":ingress" + str(host_obj.switch_port_attached))
-            host_obj.switch_egress_port = self.ring_npg.get_node(host_obj.switch_id +
-                                                                 ":egress" + str(host_obj.switch_port_attached))
-        # Initialize traffic from each host node as well
-        for host_id in self.ng.host_ids:
-            host_obj = self.ng.get_node_object(host_id)
+            host_obj = ng.get_node_object(host_id)
+            host_obj.switch_ingress_port = npg.get_node(host_obj.switch_id +
+                                                        ":ingress" +
+                                                        str(host_obj.switch_port_attached))
+            host_obj.switch_egress_port = npg.get_node(host_obj.switch_id +
+                                                       ":egress" +
+                                                       str(host_obj.switch_port_attached))
+    @classmethod
+    def init_hosts_traffic_propagation(cls, ng, npg):
+        for host_id in ng.host_ids:
+            host_obj = ng.get_node_object(host_id)
 
             dst_traffic_at_succ = Traffic(init_wildcard=True)
             dst_traffic_at_succ.set_field("ethernet_type", 0x0800)
@@ -46,17 +35,33 @@ class TestNetworkPortGraph(unittest.TestCase):
 
             end_to_end_modified_edges = []
 
-            self.ring_npg.propagate_admitted_traffic(host_obj.switch_egress_port,
-                                                     dst_traffic_at_succ,
-                                                     None,
-                                                     host_obj.switch_egress_port,
-                                                     end_to_end_modified_edges)
+            npg.propagate_admitted_traffic(host_obj.switch_egress_port,
+                                           dst_traffic_at_succ,
+                                           None,
+                                           host_obj.switch_egress_port,
+                                           end_to_end_modified_edges)
 
-    def test_two_link_failure_modification_change(self):
+    @classmethod
+    def setUpClass(cls):
 
-        # Initialize some data structures
-        src_h_obj = self.ng.get_node_object("h21")
-        dst_h_obj = self.ng.get_node_object("h31")
+        cls.nc_ring_aborescene_apply_true = NetworkConfiguration("ryu",
+                                                                 "ring",
+                                                                 {"num_switches": 4,
+                                                                  "num_hosts_per_switch": 1},
+                                                                 conf_root=os.path.dirname(__file__) + "/",
+                                                                 synthesis_name="AboresceneSynthesis",
+                                                                 synthesis_params={"apply_group_intents_immediately":
+                                                                                       True})
+
+        cls.ng_ring_aborescene_apply_true = cls.nc_ring_aborescene_apply_true.setup_network_graph(mininet_setup_gap=1,
+                                                                                                  synthesis_setup_gap=1)
+        cls.npg_ring_aborescene_apply_true = NetworkPortGraph(cls.ng_ring_aborescene_apply_true, True)
+        cls.npg_ring_aborescene_apply_true.init_network_port_graph()
+
+        cls.attach_hosts_port_nodes_with_npg(cls.ng_ring_aborescene_apply_true, cls.npg_ring_aborescene_apply_true)
+        cls.init_hosts_traffic_propagation(cls.ng_ring_aborescene_apply_true, cls.npg_ring_aborescene_apply_true)
+
+    def check_two_link_failure_admitted_traffic_absence(self, npg, src_h_obj, dst_h_obj, links_to_fail):
 
         specific_traffic = Traffic(init_wildcard=True)
         specific_traffic.set_field("ethernet_type", 0x0800)
@@ -66,27 +71,23 @@ class TestNetworkPortGraph(unittest.TestCase):
         specific_traffic.set_field("vlan_id", src_h_obj.switch_obj.synthesis_tag + 0x1000, is_exception_value=True)
         specific_traffic.set_field("has_vlan_tag", 0)
 
-        src_node = self.ring_npg.get_ingress_node("s2", 1)
-        dst_node = self.ring_npg.get_egress_node("s3", 1)
-        egress_node_2 = self.ring_npg.get_egress_node("s2", 2)
-        egress_node_3 = self.ring_npg.get_egress_node("s2", 3)
+        before_at = npg.get_admitted_traffic(src_h_obj.switch_ingress_port, dst_h_obj.switch_egress_port)
 
-        # Delete the first link and measure
-        self.ring_npg.remove_node_graph_link("s1", "s4")
-        before_at = self.ring_npg.get_admitted_traffic_via_succ(src_node, dst_node, egress_node_3)
-        before_at_int = specific_traffic.intersect(before_at)
-        before_vlan_tag_modification = before_at_int.traffic_elements[0].switch_modifications["vlan_id"][1]
+        for link_to_fail in links_to_fail:
+            npg.remove_node_graph_link(*link_to_fail)
 
-        # Delete the second link
-        self.ring_npg.remove_node_graph_link("s2", "s3")
-        after_at = self.ring_npg.get_admitted_traffic_via_succ(src_node, dst_node, egress_node_2)
-        after_at_int = specific_traffic.intersect(after_at)
-        after_vlan_tag_modification = after_at_int.traffic_elements[0].switch_modifications["vlan_id"][1]
+        after_at = npg.get_admitted_traffic(src_h_obj.switch_ingress_port, dst_h_obj.switch_egress_port)
+        self.assertEqual(after_at.is_empty(), True)
 
-        self.assertNotEqual(before_vlan_tag_modification, after_vlan_tag_modification)
+    def test_two_link_failure_admitted_traffic_absence(self):
 
-    def tearDown(self):
-        self.nc.cm.stop_controller()
+        # Initialize some data structures
+        src_h_obj = self.ng_ring_aborescene_apply_true.get_node_object("h21")
+        dst_h_obj = self.ng_ring_aborescene_apply_true.get_node_object("h31")
+        links_to_fail = [("s1", "s4"), ("s2", "s3")]
+
+        self.check_two_link_failure_admitted_traffic_absence(self.npg_ring_aborescene_apply_true,
+                                                             src_h_obj, dst_h_obj, links_to_fail)
 
 if __name__ == '__main__':
     unittest.main()
