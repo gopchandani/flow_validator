06c1a471ed61958fb484270c2ebe66740163a12e
Rakesh Kumar <gopchandani@gmail.com>
2016-08-15 09:21:11 -0700

    Another merge.

--- src/experiments/initial_incremental_policy_times.py
+++ src/experiments/initial_incremental_policy_times.py
@@ -333,6 +333,20 @@ class InitialIncrementalTimes(Experiment):
 
         return merged_data
 
+    def data_merge_2(self):
+
+        path_prefix = "data_merge/"
+
+        eight_switch_ring_merge = self.load_data(path_prefix + "8_switch_ring.json")
+        seven_switch_clos_merge = self.load_data(path_prefix + "7_switch_clos.json")
+        fourteen_switch_clos_merge = self.load_data(path_prefix + "14_switch_clos.json")
+
+        merged_data = self.load_data_merge_network_config([eight_switch_ring_merge,
+                                                           seven_switch_clos_merge,
+                                                           fourteen_switch_clos_merge])
+
+        return merged_data
+
 
 def prepare_network_configurations(num_hosts_per_switch_list):
     nc_list = []
@@ -349,7 +363,7 @@ def prepare_network_configurations(num_hosts_per_switch_list):
 
         nc = NetworkConfiguration("ryu",
                                  "ring",
-                                 {"num_switches": 8,
+                                 {"num_switches": 12,
                                   "num_hosts_per_switch": hps},
                                  conf_root="configurations/",
                                  synthesis_name="AboresceneSynthesis",
@@ -365,23 +379,25 @@ def prepare_network_configurations(num_hosts_per_switch_list):
 def main():
 
     num_iterations = 2
-    link_fraction_to_sample = 1.0
-    num_hosts_per_switch_list = [5]#, 4]#[2, 4]#, 6, 8, 10]
+    link_fraction_to_sample = 0.25
+    num_hosts_per_switch_list = [2, 4, 6, 8, 10]
     network_configurations = prepare_network_configurations(num_hosts_per_switch_list)
     exp = InitialIncrementalTimes(num_iterations,
                                   link_fraction_to_sample,
                                   network_configurations)
 
     # Trigger the experiment
-    exp.trigger()
+    #exp.trigger()
     #exp.dump_data()
 
     #exp.load_data("data/sgc_merged_data.json")
 
-    exp.data = exp.generate_relative_cost_ratio_data(exp.data)
-    exp.data = exp.generate_num_flow_path_keys(exp.data)
-
-    #exp.plot_initial_incremental_times()
+    # exp.data = exp.data_merge_2()
+    # exp.data = exp.generate_relative_cost_ratio_data(exp.data)
+    # exp.data = exp.generate_num_flow_path_keys(exp.data)
+    # exp.dump_data()
+    #
+    # exp.plot_initial_incremental_times()
 
 if __name__ == "__main__":
     main()
