eb5ad8d8354c0b98ca66cbbd6f70e9f9366cc6c8
Rakesh Kumar <kumar19@illinois.edu>
2016-11-30 14:54:05 -0800

    Shaping up what the experiments ought to look like.

--- src/experiments/substation_mixed_policy_validation_times.py
+++ src/experiments/substation_mixed_policy_validation_times.py
@@ -19,81 +19,105 @@ from analysis.policy_statement import CONNECTIVITY_CONSTRAINT, PATH_LENGTH_CONST
 class SubstationMixedPolicyValidationTimes(Experiment):
 
     def __init__(self,
-                 network_configuration,
+                 network_configurations,
                  num_iterations):
 
         super(SubstationMixedPolicyValidationTimes, self).__init__("resiliency_policy_times", 1)
 
-        self.network_configuration = network_configuration
+        self.network_configurations = network_configurations
         self.num_iterations = num_iterations
 
         self.data = {
             "validation_time": defaultdict(list)
         }
 
+    def construct_policy_statements(self, nc):
+
+        s1_src_zone = [nc.ng.get_node_object("h21").switch_port,
+                       nc.ng.get_node_object("h31").switch_port]
+
+        s1_dst_zone = [nc.ng.get_node_object("h11").switch_port]
+
+        s1_traffic = Traffic(init_wildcard=True)
+        s1_traffic.set_field("ethernet_type", 0x0800)
+        s1_traffic.set_field("has_vlan_tag", 0)
+
+        s1_constraints = [PolicyConstraint(CONNECTIVITY_CONSTRAINT, None),
+                          PolicyConstraint(PATH_LENGTH_CONSTRAINT, 6),
+                          PolicyConstraint(LINK_EXCLUSIVITY_CONSTRAINT, [("s1", "s2")])]
+        s1_k = 1
+        s1 = PolicyStatement(nc.ng, s1_src_zone, s1_dst_zone, s1_traffic, s1_constraints, s1_k)
+
+        s2_src_zone = [nc.ng.get_node_object("h41").switch_port]
+
+        s2_dst_zone = [nc.ng.get_node_object("h11").switch_port]
+
+        s2_traffic = Traffic(init_wildcard=True)
+        s2_traffic.set_field("ethernet_type", 0x0800)
+        s2_traffic.set_field("has_vlan_tag", 0)
+        s2_traffic.set_field("tcp_destination_port", 443)
+
+        s2_constraints = [PolicyConstraint(CONNECTIVITY_CONSTRAINT, None)]
+        s2_k = 0
+        s2 = PolicyStatement(nc.ng, s2_src_zone, s2_dst_zone, s2_traffic, s2_constraints, s2_k)
+
+        return [s1, s2]
+
     def trigger(self):
 
-        ng = self.network_configuration.setup_network_graph(mininet_setup_gap=1, synthesis_setup_gap=1)
+        for nc in self.network_configurations:
 
-        for i in range(self.num_iterations):
+            for i in range(self.num_iterations):
 
-            with Timer(verbose=True) as t:
+                policy_statements = self.construct_policy_statements(nc)
 
-                s1_src_zone = [ng.get_node_object("h21").switch_port,
-                               ng.get_node_object("h31").switch_port]
+                with Timer(verbose=True) as t:
+                    fv = FlowValidator(nc.ng)
+                    fv.init_network_port_graph()
+                    satisfies = fv.validate_policy(policy_statements)
 
-                s1_dst_zone = [ng.get_node_object("h11").switch_port]
-
-                s1_traffic = Traffic(init_wildcard=True)
-                s1_traffic.set_field("ethernet_type", 0x0800)
-                s1_traffic.set_field("has_vlan_tag", 0)
-
-                s1_constraints = [PolicyConstraint(CONNECTIVITY_CONSTRAINT, None),
-                                  PolicyConstraint(PATH_LENGTH_CONSTRAINT, 6),
-                                  PolicyConstraint(LINK_EXCLUSIVITY_CONSTRAINT, [("s1", "s2")])]
-                s1_k = 2
-                s1 = PolicyStatement(ng, s1_src_zone, s1_dst_zone, s1_traffic, s1_constraints, s1_k)
-
-                s2_src_zone = [ng.get_node_object("h41").switch_port]
-
-                s2_dst_zone = [ng.get_node_object("h11").switch_port]
-
-                s2_traffic = Traffic(init_wildcard=True)
-                s2_traffic.set_field("ethernet_type", 0x0800)
-                s2_traffic.set_field("has_vlan_tag", 0)
-                s2_traffic.set_field("tcp_destination_port", 443)
-
-                s2_constraints = [PolicyConstraint(CONNECTIVITY_CONSTRAINT, None)]
-                s2_k = 0
-                s2 = PolicyStatement(ng, s2_src_zone, s2_dst_zone, s2_traffic, s2_constraints, s2_k)
-
-                fv = FlowValidator(ng)
-                fv.init_network_port_graph()
-                satisfies = fv.validate_policy([s1, s2])
                 print "Does the network configuration satisfies the given policy:", satisfies
+                self.data["validation_time"][nc.nc_topo_str].append(t.secs)
 
-            self.data["validation_time"][self.network_configuration.nc_topo_str].append(t.secs)
+
+def prepare_network_configurations(num_switches_in_clique_list, num_hosts_per_switch_list):
+    nc_list = []
+
+    for nsc in num_switches_in_clique_list:
+
+        for hps in num_hosts_per_switch_list:
+
+            nc = NetworkConfiguration("ryu",
+                                      "127.0.0.1",
+                                      6633,
+                                      "http://localhost:8080/",
+                                      "admin",
+                                      "admin",
+                                      "cliquetopo",
+                                      {"num_switches": nsc,
+                                       "num_hosts_per_switch": hps,
+                                       "per_switch_links": nsc - 1},
+                                      conf_root="configurations/",
+                                      synthesis_name="AboresceneSynthesis",
+                                      synthesis_params={"apply_group_intents_immediately": True})
+
+            nc.setup_network_graph(mininet_setup_gap=1, synthesis_setup_gap=1)
+
+            nc_list.append(nc)
+
+    return nc_list
 
 
 def main():
 
-    num_iterations = 1
+    num_iterations = 5
 
-    network_configuration = NetworkConfiguration("ryu",
-                                                 "127.0.0.1",
-                                                 6633,
-                                                 "http://localhost:8080/",
-                                                 "admin",
-                                                 "admin",
-                                                 "cliquetopo",
-                                                 {"num_switches": 4,
-                                                  "num_hosts_per_switch": 1,
-                                                  "per_switch_links": 3},
-                                                 conf_root="configurations/",
-                                                 synthesis_name="AboresceneSynthesis",
-                                                 synthesis_params={"apply_group_intents_immediately": True})
+    num_switches_in_clique_list = [4, 5]
+    num_hosts_per_switch_list = [1]
 
-    exp = SubstationMixedPolicyValidationTimes(network_configuration, num_iterations)
+    network_configurations = prepare_network_configurations(num_switches_in_clique_list, num_hosts_per_switch_list)
+
+    exp = SubstationMixedPolicyValidationTimes(network_configurations, num_iterations)
     exp.trigger()
     exp.dump_data()
 
