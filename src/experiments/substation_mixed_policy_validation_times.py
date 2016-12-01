d5b4cff45a6d5fb27afde0e27c85b640dc89e841
Rakesh Kumar <kumar19@illinois.edu>
2016-11-30 16:47:40 -0800

    Adding the knob of s1_k_values

--- src/experiments/substation_mixed_policy_validation_times.py
+++ src/experiments/substation_mixed_policy_validation_times.py
@@ -20,18 +20,20 @@ class SubstationMixedPolicyValidationTimes(Experiment):
 
     def __init__(self,
                  network_configurations,
+                 s1_k_values,
                  num_iterations):
 
         super(SubstationMixedPolicyValidationTimes, self).__init__("resiliency_policy_times", 1)
 
         self.network_configurations = network_configurations
+        self.s1_k_values = s1_k_values
         self.num_iterations = num_iterations
 
         self.data = {
             "validation_time": defaultdict(list)
         }
 
-    def construct_policy_statements(self, nc):
+    def construct_policy_statements(self, nc, s1_k):
 
         s1_src_zone = [nc.ng.get_node_object("h21").switch_port,
                        nc.ng.get_node_object("h31").switch_port]
@@ -45,7 +47,7 @@ class SubstationMixedPolicyValidationTimes(Experiment):
         s1_constraints = [PolicyConstraint(CONNECTIVITY_CONSTRAINT, None),
                           PolicyConstraint(PATH_LENGTH_CONSTRAINT, 6),
                           PolicyConstraint(LINK_EXCLUSIVITY_CONSTRAINT, [("s1", "s2")])]
-        s1_k = 1
+
         s1 = PolicyStatement(nc.ng, s1_src_zone, s1_dst_zone, s1_traffic, s1_constraints, s1_k)
 
         s2_src_zone = [nc.ng.get_node_object("h41").switch_port]
@@ -66,18 +68,19 @@ class SubstationMixedPolicyValidationTimes(Experiment):
     def trigger(self):
 
         for nc in self.network_configurations:
+            for s1_k in self.s1_k_values:
 
-            for i in range(self.num_iterations):
+                for i in range(self.num_iterations):
 
-                policy_statements = self.construct_policy_statements(nc)
+                    policy_statements = self.construct_policy_statements(nc, s1_k)
 
-                with Timer(verbose=True) as t:
-                    fv = FlowValidator(nc.ng)
-                    fv.init_network_port_graph()
-                    satisfies = fv.validate_policy(policy_statements)
+                    with Timer(verbose=True) as t:
+                        fv = FlowValidator(nc.ng)
+                        fv.init_network_port_graph()
+                        satisfies = fv.validate_policy(policy_statements)
 
-                print "Does the network configuration satisfies the given policy:", satisfies
-                self.data["validation_time"][nc.nc_topo_str].append(t.secs)
+                    print "Does the network configuration satisfies the given policy:", satisfies
+                    self.data["validation_time"][nc.nc_topo_str].append(t.secs)
 
 
 def prepare_network_configurations(num_switches_in_clique_list, num_hosts_per_switch_list):
@@ -110,14 +113,15 @@ def prepare_network_configurations(num_switches_in_clique_list, num_hosts_per_sw
 
 def main():
 
-    num_iterations = 5
+    num_iterations = 1
 
-    num_switches_in_clique_list = [4, 5]
+    num_switches_in_clique_list = [4]
     num_hosts_per_switch_list = [1]
+    s1_k_values = [1, 2]
 
     network_configurations = prepare_network_configurations(num_switches_in_clique_list, num_hosts_per_switch_list)
 
-    exp = SubstationMixedPolicyValidationTimes(network_configurations, num_iterations)
+    exp = SubstationMixedPolicyValidationTimes(network_configurations, s1_k_values, num_iterations)
     exp.trigger()
     exp.dump_data()
 
