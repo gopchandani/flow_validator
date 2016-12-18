ded3e82a233ff6f658f818f5a9faf0b385e8e984
Rakesh Kumar <kumar19@illinois.edu>
2016-12-18 11:25:53 -0800

    Merge.

--- src/experiments/substation_mixed_policy_validation_times.py
+++ src/experiments/substation_mixed_policy_validation_times.py
@@ -1,6 +1,8 @@
 import sys
+import json
 
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 
 from collections import defaultdict
 
@@ -104,14 +106,97 @@ class SubstationMixedPolicyValidationTimes(Experiment):
 
                     self.dump_data()
 
+    def plot_lines_with_error_bars(self,
+                                   ax,
+                                   data_key,
+                                   x_label,
+                                   y_label,
+                                   subplot_title,
+                                   y_scale,
+                                   x_min_factor=1.0,
+                                   x_max_factor=1.05,
+                                   y_min_factor=0.1,
+                                   y_max_factor=1.5,
+                                   xticks=None,
+                                   xtick_labels=None,
+                                   yticks=None,
+                                   ytick_labels=None):
+
+        ax.set_xlabel(x_label, fontsize=11, labelpad=-0)
+        ax.set_ylabel(y_label, fontsize=11, labelpad=0)
+        ax.set_title(subplot_title, fontsize=12)
+
+        markers = ['.', 'x', '^', 'v', '*', '+', 'H', 's']
+        marker_i = 0
+
+        for line_data_key in sorted(self.data[data_key].keys()):
+
+            style = None
+
+            if line_data_key.find("0") == 3:
+                style = "dotted"
+            elif line_data_key.find("1") == 3:
+                style = "dashed"
+            elif line_data_key.find("2") == 3:
+                style = "dashdot"
+            elif line_data_key.find("3") == 3:
+                style = "solid"
+
+            data_vals = self.data[data_key][line_data_key]
+
+            x, mean, sem = self.prepare_matplotlib_data(data_vals)
+
+            ax.errorbar(x,
+                        mean,
+                        sem,
+                        color="black",
+                        marker=markers[marker_i],
+                        markersize=7.0,
+                        label=line_data_key,
+                        ls="none")
+
+            marker_i += 1
+
+        ax.tick_params(axis='x', labelsize=11)
+        ax.tick_params(axis='y', labelsize=11)
+
+        low_xlim, high_xlim = ax.get_xlim()
+        ax.set_xlim(xmax=(high_xlim) * x_max_factor)
+        ax.set_xlim(xmin=(low_xlim) * x_min_factor)
+
+        if y_scale == "linear":
+            low_ylim, high_ylim = ax.get_ylim()
+            ax.set_ylim(ymin=low_ylim*y_min_factor)
+            ax.set_ylim(ymax=high_ylim*y_max_factor)
+        elif y_scale == "log":
+            ax.set_ylim(ymin=2)
+            ax.set_ylim(ymax=100000)
+
+        ax.set_yscale(y_scale)
+
+        xa = ax.get_xaxis()
+        xa.set_major_locator(MaxNLocator(integer=True))
+
+        if xticks:
+            ax.set_xticks(xticks)
+
+        if xtick_labels:
+            ax.set_xticklabels(xtick_labels)
+
+        if yticks:
+            ax.set_yticks(yticks)
+
+        if ytick_labels:
+            ax.set_yticklabels(ytick_labels)
+
     def plot_data(self):
 
-        f, (ax1) = plt.subplots(1, 1, sharex=True, sharey=False, figsize=(4.5, 4.5))
+        f, (ax1) = plt.subplots(1, 1, sharex=True, sharey=False, figsize=(5.0, 4.0))
 
         data_xtick_labels = self.data["validation_time"]["k: 0, |L|: 4"].keys()
         data_xticks = [int(x) for x in data_xtick_labels]
 
-        self.plot_lines_with_error_bars_2(ax1,
+        self.plot_lines_with_error_bars(ax1,
                                         "validation_time",
                                         "Number of host pairs",
                                         "Time (seconds)",
@@ -125,29 +210,47 @@ class SubstationMixedPolicyValidationTimes(Experiment):
                                         xtick_labels=data_xtick_labels)
 
         xlabels = ax1.get_xticklabels()
-        plt.setp(xlabels, rotation=45, fontsize=10)
+        plt.setp(xlabels, rotation=0, fontsize=10)
 
         # Shrink current axis's height by 25% on the bottom
         box = ax1.get_position()
-        ax1.set_position([box.x0, box.y0 + box.height * 0.3,
-                          box.width, box.height * 0.7])
-
+        ax1.set_position([box.x0, box.y0 + box.height * 0.3, box.width, box.height * 0.7])
         handles, labels = ax1.get_legend_handles_labels()
 
-        ax1.legend(handles,
-                   labels,
-                   shadow=True,
-                   fontsize=10,
-                   loc='upper center',
-                   ncol=2,
-                   markerscale=1.0,
-                   frameon=True,
-                   fancybox=True,
-                   columnspacing=2.5,
-                   bbox_to_anchor=[0.5, -0.20])
+        ax1.legend(handles, labels, shadow=True, fontsize=10, loc='upper center', ncol=4, markerscale=1.0,
+                   frameon=True, fancybox=True, columnspacing=0.5, bbox_to_anchor=[0.5, -0.25])
 
         plt.savefig("plots/" + self.experiment_tag + "_substation_mixed_policy_validation_times" + ".png", dpi=1000)
         plt.show()
+
+    def load_data_merge_iterations(self, filename_list):
+
+        '''
+        :param filename_list: List of files with exact same network configurations in them
+        :return: merged data
+        '''
+
+        merged_data = None
+
+        for filename in filename_list:
+
+            print "Reading file:", filename
+
+            with open(filename, "r") as infile:
+                this_data = json.load(infile)
+
+            if merged_data:
+                for ds in merged_data:
+                    for case in merged_data[ds]:
+                        for num_conns in merged_data[ds][case]:
+                            try:
+                                merged_data[ds][case][num_conns].extend(this_data[ds][case][num_conns])
+                            except KeyError:
+                                print num_conns, "not found."
+            else:
+                merged_data = this_data
+
+        return merged_data
 
 
 def prepare_network_configurations(num_switches_in_clique_list, num_hosts_per_switch_list, num_per_switch_links_list):
@@ -196,7 +299,15 @@ def main():
     # exp.trigger()
     # exp.dump_data()
 
-    exp.load_data("data/substation_mixed_policy_validation_times_1_iterations_20161214_143932.json")
+    #exp.load_data("data/substation_mixed_policy_validation_times_1_iterations_20161216_112622.json")
+
+    #exp.load_data("data/substation_mixed_policy_validation_times_1_iterations_20161214_182509.json")
+
+    exp.data = exp.load_data_merge_iterations(
+        ["data/substation_mixed_policy_validation_times_1_iterations_20161214_182509.json",
+         "data/substation_mixed_policy_validation_times_1_iterations_20161216_112622.json"
+         ])
+
     exp.plot_data()
 
 if __name__ == "__main__":
