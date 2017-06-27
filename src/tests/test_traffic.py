6c8468786bed53fb42c61f46a4e2eda3ee213b47
Rakesh Kumar <kumar19@illinois.edu>
2017-06-27 14:13:13 -0700

    One more test.

--- src/tests/test_traffic.py
+++ src/tests/test_traffic.py
@@ -10,7 +10,7 @@ class TestTraffic(unittest.TestCase):
     def setUpClass(cls):
         cls.wildcard_traffic = Traffic(init_wildcard=True)
 
-        cls.non_wildcard_traffic_per_field = dict()
+        cls.one_field_specific_traffic_per_field = dict()
         for field_name in field_names:
             t = Traffic(init_wildcard=True)
 
@@ -20,15 +20,21 @@ class TestTraffic(unittest.TestCase):
             else:
                 t.traffic_elements[0].set_traffic_field(field_name, 1)
 
-            cls.non_wildcard_traffic_per_field[field_name] = t
+            cls.one_field_specific_traffic_per_field[field_name] = t
 
     def test_wildcard_intersection(self):
         at_int = self.wildcard_traffic.intersect(self.wildcard_traffic)
         self.assertEqual(at_int.is_wildcard(), True)
 
         for field_name in field_names:
-            at_int = self.wildcard_traffic.intersect(self.non_wildcard_traffic_per_field[field_name])
+            at_int = self.wildcard_traffic.intersect(self.one_field_specific_traffic_per_field[field_name])
             self.assertEqual(at_int.is_wildcard(), False)
+
+    def test_one_field_specific_intersection(self):
+        for t1 in self.one_field_specific_traffic_per_field.values():
+            for t2 in self.one_field_specific_traffic_per_field.values():
+                at_int = t1.intersect(t2)
+                self.assertEqual(at_int.is_empty(), False)
 
 if __name__ == '__main__':
     unittest.main()
