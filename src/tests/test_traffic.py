41fe7a9e55c2731a31cf33c56f646278c47b0c6e
Rakesh Kumar <kumar19@illinois.edu>
2017-06-28 10:26:09 -0700

    More tests for is_subset now.

--- src/tests/test_traffic.py
+++ src/tests/test_traffic.py
@@ -1,4 +1,5 @@
 import unittest
+import itertools
 from netaddr import IPNetwork
 from model.traffic import Traffic
 from model.match import field_names
@@ -26,38 +27,55 @@ class TestTraffic(unittest.TestCase):
     def setUpClass(cls):
         cls.wildcard_traffic = Traffic(init_wildcard=True)
 
-        cls.field_specific_traffic_per_field_1 = get_specific_traffic_per_field_dict(1, IPNetwork("192.168.1.1"))
-        cls.field_specific_traffic_per_field_2 = get_specific_traffic_per_field_dict(2, IPNetwork("192.168.1.2"))
+        cls.specific_traffic_per_field_1 = get_specific_traffic_per_field_dict(1, IPNetwork("192.168.1.1"))
+        cls.specific_traffic_per_field_2 = get_specific_traffic_per_field_dict(2, IPNetwork("192.168.1.2"))
 
-    def test_wildcard_intersection(self):
+    def test_intersection_wildcard(self):
         at_int = self.wildcard_traffic.intersect(self.wildcard_traffic)
-        self.assertEqual(at_int.is_wildcard(), True)
+        self.assertEqual(at_int.is_wildcard(), 
+                         True)
 
         for field_name in field_names:
-            at_int = self.wildcard_traffic.intersect(self.field_specific_traffic_per_field_1[field_name])
-            self.assertEqual(at_int.is_wildcard(), False)
+            at_int = self.wildcard_traffic.intersect(self.specific_traffic_per_field_1[field_name])
+            self.assertEqual(at_int.is_wildcard(), 
+                             False)
 
-    def test_positive_traffic_intersection_per_field(self):
+    def test_intersection_positive_traffic_per_field(self):
+        for field_1, field_2 in itertools.combinations(field_names, 2):
+            t1 = self.specific_traffic_per_field_1[field_1]
+            t2 = self.specific_traffic_per_field_1[field_2]
+            at_int = t1.intersect(t2)
+            self.assertEqual(at_int.is_empty(), False)
 
-        for field_1 in field_names:
-            t1 = self.field_specific_traffic_per_field_1[field_1]
-
-            for field_2 in field_names:
-                t2 = self.field_specific_traffic_per_field_1[field_2]
-
+    def test_intersection_negative_traffic_per_field(self):
+        for field_1, field_2 in itertools.combinations(field_names, 2):
+            t1 = self.specific_traffic_per_field_1[field_1]
+            t2 = self.specific_traffic_per_field_2[field_2]
+            if field_1 == field_2:
                 at_int = t1.intersect(t2)
-                self.assertEqual(at_int.is_empty(), False)
+                self.assertEqual(at_int.is_empty(), True)
 
-    def test_negative_traffic_intersection_per_field(self):
-        for field_1 in field_names:
-            t1 = self.field_specific_traffic_per_field_1[field_1]
+    def test_subset_wildcard(self):
+        self.assertEqual(self.wildcard_traffic.is_subset_traffic(self.wildcard_traffic), 
+                         True)
 
-            for field_2 in field_names:
-                t2 = self.field_specific_traffic_per_field_2[field_2]
+        for field_name in field_names:
+            self.assertEqual(self.wildcard_traffic.is_subset_traffic(self.specific_traffic_per_field_1[field_name]),
+                             True)
 
-                if field_1 == field_2:
-                    at_int = t1.intersect(t2)
-                    self.assertEqual(at_int.is_empty(), True)
+    def test_subset_positive_traffic_per_field(self):
+        for field_1, field_2 in itertools.combinations(field_names, 2):
+            if field_1 == field_2:
+                t1 = self.specific_traffic_per_field_1[field_1]
+                t2 = self.specific_traffic_per_field_1[field_2]
+                self.assertEqual(t1.is_subset_traffic(t2), True)
+
+    def test_subset_negative_traffic_per_field(self):
+        for field_1, field_2 in itertools.combinations(field_names, 2):
+            if field_1 == field_2:
+                t1 = self.specific_traffic_per_field_1[field_1]
+                t2 = self.specific_traffic_per_field_2[field_2]
+                self.assertEqual(t1.is_subset_traffic(t2), False)
 
 if __name__ == '__main__':
     unittest.main()
