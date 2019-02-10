09f114706ea7b5d69bd9b3f81e1e1f84ba535728
Rakesh Kumar <kumar19@illinois.edu>
2019-02-09 22:01:46 -0600

    Adding bazel stuff and a simple example.

---
+++ flow_validator_cpp_backend/src/main.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <boost/algorithm/string.hpp>
+#include "proto/flow_validator.pb.h"
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    cout << "Hello again!" << endl;
+    
+    return 0;
+
+}
