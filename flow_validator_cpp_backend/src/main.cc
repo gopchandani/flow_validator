4ad871460ced7e2aece7d4c74a141b4b70eb1fcd
Rakesh Kumar <kumar19@illinois.edu>
2019-02-11 12:22:58 -0600

    Got graphs to work

--- flow_validator_cpp_backend/src/main.cc
+++ flow_validator_cpp_backend/src/main.cc
@@ -9,7 +9,21 @@
 #include <grpcpp/security/server_credentials.h>
 #include "proto/flow_validator.grpc.pb.h"
 
+#include "boost/graph/adjacency_list.hpp"
+#include "boost/graph/graphviz.hpp"
+
 using namespace std;
+
+using namespace boost;
+
+/* define the graph type
+      listS: selects the STL list container to store 
+            the OutEdge list
+      vecS: selects the STL vector container to store 
+            the vertices
+      directedS: selects directed edges
+*/
+typedef adjacency_list< listS, vecS, directedS > digraph;
 
 using grpc::Server;
 using grpc::ServerBuilder;
@@ -32,11 +46,34 @@ class FlowValidatorImpl final : public FlowValidator::Service {
 
   }
 
+  void adjacency_list_example() {
+
+
+      // instantiate a digraph object with 8 vertices
+      digraph g(8);
+
+      // add some edges
+      add_edge(0, 1, g);
+      add_edge(1, 5, g);
+      add_edge(5, 6, g);
+      add_edge(2, 3, g);
+      add_edge(2, 4, g);
+      add_edge(3, 5, g);
+      add_edge(4, 5, g);
+      add_edge(5, 7, g);
+
+      // represent graph in DOT format and send to cout
+      write_graphviz(cout, g);
+
+  }
+
   Status Initialize(ServerContext* context, const NetworkGraph* ng, InitializeInfo* info) override {
     cout << "Received Initialize request" << endl;
 
     info->set_successful(true);
     info->set_time_taken(0.1);
+
+    adjacency_list_example();
 
     return Status::OK;
   }
